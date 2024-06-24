54f2ac79f18af2d2ddc360ae3a5f54797539f710
Gage Goodwin <gagegoodwin123@gmail.com>
2024-06-23 19:51:13 -0500

    spring initializr java 21 app

---
+++ src/main/java/com/api/pingpong/PingPong.java
@@ -0,0 +1,13 @@
+package com.api.pingpong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingPong {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingPong.class, args);
+	}
+
+}
